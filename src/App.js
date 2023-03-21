a703ef9ef9d98589b420cf0decb768ff7cb71ed9
Varandeep07 <varandeepsahota@gmail.com>
2023-03-21 15:22:33 +0530

    Added About

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
       <Navbar title = "TextUtils" mode = {mode} toggleMode = {toggleMode}/>
       <Alert alert = {alert}/>
       <div className="container my-3">
-        {/* <About/>  */}
+        <About/> 
         <TextForm showAlert = {showAlert} heading = "Enter the text to analyze below" mode = {mode}/> 
       </div>
     </>
